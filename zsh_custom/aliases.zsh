e657ea689163437b78c2924748ccd8c7805a1ac1
Viktor Gamov <viktor@hazelcast.com>
2015-01-02 12:55:39 -0500

    updated gitconfig and aliases

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -1,3 +1,20 @@
+# Easier navigation: .., ..., ...., ....., ~ and -
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ~="cd ~" # `cd` is probably faster to type though
+alias -- -="cd -"
+
+# Shortcuts
+alias d="cd ~/Documents/Dropbox"
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias p="cd ~/projects"
+alias g="git"
+alias h="history"
+alias j="jobs"
+
 alias sys_info='sw_vers'
 alias reload!='. ~/.zshrc'
 alias c='pygmentize -O style=tomorrownighteighties -f console256 -g'
